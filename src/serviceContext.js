1d068143eef9296d17515ce3e24b7cb06f9395f0
anjuthomas <anjuthomas@google.com>
2018-05-23 22:01:53 -0700

    Revised ServiceContext 2

--- src/serviceContext.js
+++ src/serviceContext.js
@@ -2,12 +2,10 @@ const crypto = require('crypto');
 const KeyJar = require('../nodeOIDCMsg/src/oicMsg/keystore/KeyJar').KeyJar;
 
 /** 
- * ServiceContext
  * This class keeps information that a client needs to be able to talk
  * to a server. Some of this information comes from configuration and some
  * from dynamic provider info discovery or client registration.
  * But information is also picked up during the conversation with a server.
- * @class 
  */
 class ServiceContext {
   /**
@@ -16,26 +14,14 @@ class ServiceContext {
    * @param {Object<string, string>} params Other attributes that might be needed
    */
   constructor(keyjar, config, params) {
-    this.clientSecret = [this.getClientSecret, this.setClientSecret];
-    keyjar = keyjar || null;
-    config = config || null;
     this.keyjar = keyjar || new KeyJar();
     this.providerInfo = {};
     this.registrationResponse = {};
     this.kid = {'sig': {}, 'enc': {}};
     this.config = config || {};
-    this.baseUrl = '';
-    this.requestDir = '';
-    this.allow = {};
-    this.behavior = {};
-    this.clientPreferences = {};
-    this.cId = '';
-    this.cSecret = '';
-    this.issuer = '';
-    let serviceContext = ['client_id', 'issuer', 'client_secret', 'base_url', 'requests_dir'];
     let defaultVal = '';
 
-    if (params){
+    if (params) {
       for (var i = 0; i < Object.keys(params).length; i++){
         let key = Object.keys(params)[i];
         let val = params[key];
@@ -43,36 +29,18 @@ class ServiceContext {
       }
     }
 
-    for (let i = 0; i < serviceContext.length; i++) {
-      let attr = serviceContext[i];
-      if (attr === 'client_id') {
-        this.client_id = this.config[attr] || defaultVal;
-      } else if (attr === 'issuer') {
-        this.issuer = this.config[attr] || defaultVal;
-      } else if (attr === 'client_secret') {
-        this.client_secret = this.config[attr] || defaultVal;
-        this.setClientSecret(this.client_secret);
-      } else if (attr === 'base_url') {
-        this.base_url = this.config[attr] || defaultVal;
-      } else if (attr === 'requests_dir') {
-        this.request_dir = this.config[attr] || defaultVal;
-      }
-    }
+    this.client_id = this.config['client_id'] || defaultVal;
+    this.issuer = this.config['issuer'] || defaultVal;
+    this.client_secret = this.config['client_secret'] || defaultVal;
+    this.setclient_secret(this.client_secret);    
+    this.base_url = this.config['base_url'] || defaultVal;
+    this.request_dir = this.config['requests_dir'] || defaultVal;
 
-    let providerInfo = ['allow', 'client_preferences', 'behaviour', 'provider_info'];
-    defaultVal = {};
-    for (let i = 0; i < providerInfo.length; i++) {
-      let attr = providerInfo[i];
-      if (attr === 'allow') {
-        this.allow = this.config[attr] || defaultVal;
-      } else if (attr === 'client_preferences') {
-        this.client_prefs = this.config[attr] || defaultVal;
-      } else if (attr === 'behaviour') {
-        this.behavior = this.config[attr] || defaultVal;
-      } else if (attr === 'provider_info') {
-        this.provider_info = this.config[attr] || defaultVal;
-      }
-    }
+    defaultVal = {}
+    this.allow = this.config['allow'] || defaultVal;
+    this.client_prefs = this.config['client_preferences'] || defaultVal;
+    this.behavior = this.config['behaviour'] || defaultVal;
+    this.provider_info = this.config['provider_info'] || defaultVal;
 
     try {
       this.redirectUris = this.config['redirect_uris'];
@@ -93,13 +61,13 @@ class ServiceContext {
     return this;
   }
 
-  getClientSecret() {
+  getclient_secret() {
     return this.client_secret;
   }
 
-  setClientSecret(val) {
+  setclient_secret(val) {
     if (!val) {
-      this.client_secret;
+      this.client_secret = '';
     } else {
       this.client_secret = val;
       // client uses it for signing
@@ -114,7 +82,7 @@ class ServiceContext {
 
   /**
    *  Need to generate a redirect_uri path that is unique for a OP/RP combo
-      This is to counter the mix-up attack.
+   *  This is to counter the mix-up attack.
    * @param {string} path Leading path
    * @return A list of one unique URL
    */
@@ -125,11 +93,11 @@ class ServiceContext {
     } catch (error) {
       m.update(this.issuer);
     }
-    m.update(this.baseUrl);
+    m.update(this.base_url);
     if (!path.startsWith('/')){
-      return [this.baseUrl + '/' + path+ '/' +  m.digest('hex')];
+      return [this.base_url + '/' + path+ '/' +  m.digest('hex')];
     }else{
-      return [this.baseUrl + path + '/' + m.digest('hex')];
+      return [this.base_url + path + '/' + m.digest('hex')];
     }
   }
 
@@ -145,10 +113,10 @@ class ServiceContext {
    * @param {*} webName 
    */
   filenameFromWebName(webName) {
-    if (webName.startsWith(this.baseUrl) == false){
+    if (webName.startsWith(this.base_url) == false){
       console.log('ValueError');
     }
-    let name = webName.substring(this.baseUrl.length, webName.length);
+    let name = webName.substring(this.base_url.length, webName.length);
     if (name.startsWith('/')) {
       return name.substring(1, name.length);
     } else {
